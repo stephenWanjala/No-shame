a820fc125b41d6a1a810fbc923e86a8c67e71df7
wanjala stephen <stephenwanjala145@gmail.com>
2022-10-31 19:29:06 +0300

    Added A navigationHost

---
+++ app/src/main/java/com/wantech/noshame/feature_auth/presentation/NavigationHost.kt
@@ -0,0 +1,34 @@
+package com.wantech.noshame.feature_auth.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.wantech.noshame.featureMenstrualTrack.presentation.home.HomeScreen
+import com.wantech.noshame.feature_auth.presentation.forgotPassword.components.ForgotPasswordScreen
+import com.wantech.noshame.feature_auth.presentation.login.LoginScreen
+import com.wantech.noshame.feature_auth.presentation.signUp.components.SigUpScreen
+import com.wantech.noshame.feature_auth.presentation.util.Screen
+
+@Composable
+fun NavigationHost(navController: NavHostController) {
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.SignIn.route
+    ) {
+        composable(route = Screen.SignIn.route) {
+            LoginScreen(navController)
+        }
+        composable(route = Screen.SignUp.route) {
+            SigUpScreen(navController = navController)
+        }
+
+        composable(route = Screen.Home.route) {
+            HomeScreen(navController = navController)
+        }
+        composable(route = Screen.ForgotPassword.route) {
+            ForgotPasswordScreen(navController = navController)
+        }
+    }
+}
