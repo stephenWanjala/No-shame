537d2f40badf0a80cf862366518d69345c0ee0d8
wanjala stephen <stephenwanjala145@gmail.com>
2022-11-03 14:34:17 +0300

    spinner

---
+++ app/src/main/java/com/wantech/noshame/feature_auth/presentation/components/SpinnerSample.kt
@@ -0,0 +1,96 @@
+package com.wantech.noshame.feature_auth.presentation.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowDropDown
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpinnerSample(
+    list: List<MyData>,
+    preselected: MyData,
+    onSelectionChanged: (myData: MyData) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    var selected by remember { mutableStateOf(preselected) }
+    var expanded by remember { mutableStateOf(false) } // initial value
+
+    OutlinedCard(
+        modifier = modifier.clickable {
+            expanded = !expanded
+        }
+    ) {
+
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Top,
+        ) {
+
+            Text(
+                text = selected.name,
+                modifier = Modifier.weight(1f)
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+            )
+            Icon(Icons.Outlined.ArrowDropDown, null, modifier = Modifier.padding(8.dp))
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.fillMaxWidth()   // delete this modifier and use .wrapContentWidth() if you would like to wrap the dropdown menu around the content
+            ) {
+                list.forEach { listEntry ->
+
+                    DropdownMenuItem(
+                        onClick = {
+                            selected = listEntry
+                            expanded = false
+                            onSelectionChanged(selected)
+                        },
+                        text = {
+                            Text(
+                                text = listEntry.name,
+                                modifier = Modifier
+                                    //.wrapContentWidth()  //optional instad of fillMaxWidth
+                                    .fillMaxWidth()
+                                    .align(Alignment.Start)
+                            )
+                        },
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SpinnerSample_Preview() {
+    MaterialTheme {
+        val myData = listOf(MyData(0, "Apples"), MyData(1, "Bananas"), MyData(2, "Kiwis"))
+
+        SpinnerSample(
+            myData,
+            preselected = myData.first(),
+            onSelectionChanged = { },
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+data class MyData (
+    val id: Int,
+    val name: String
+)
