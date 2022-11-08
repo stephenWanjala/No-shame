bc18d8ad26017d5d4410cb957b8f16429641ae56
wanjala stephen <stephenwanjala145@gmail.com>
2022-11-08 11:09:13 +0300

    home topAppbar

--- app/src/main/java/com/wantech/noshame/featureMenstrualTrack/presentation/home/components/HomeTopBar.kt
+++ app/src/main/java/com/wantech/noshame/featureMenstrualTrack/presentation/home/components/HomeTopBar.kt
@@ -1,34 +1,37 @@
 package com.wantech.noshame.featureMenstrualTrack.presentation.home.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeTopBar(
-    modifier: Modifier = Modifier,
-    tittle: String,
-    navIcon: ImageVector
+    modifier: Modifier = Modifier, tittle: String, navIcon: ImageVector
 ) {
 
-    TopAppBar( modifier= Modifier.fillMaxWidth(),
-       scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(),
-       title = {
 
-       },
-        colors = TopAppBarDefaults.smallTopAppBarColors()
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp),
+    ) {
+        Text(
+            text = tittle,
+            modifier = Modifier.align(Alignment.TopStart)
+        )
 
-    )
+        Icon(
+            imageVector = navIcon, contentDescription = "Nav icon",
+            modifier = Modifier.align(Alignment.TopEnd)
+        )
+    }
+
 
 }
