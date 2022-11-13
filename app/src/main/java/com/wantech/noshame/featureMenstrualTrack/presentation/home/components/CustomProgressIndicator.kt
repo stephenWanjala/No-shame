bcc670bb06aba1bb30e3ab97d2d6b57561f7f0d4
wanjala stephen <stephenwanjala145@gmail.com>
2022-11-13 22:17:21 +0300

    add text in canvas

--- app/src/main/java/com/wantech/noshame/featureMenstrualTrack/presentation/home/components/CustomProgressIndicator.kt
+++ app/src/main/java/com/wantech/noshame/featureMenstrualTrack/presentation/home/components/CustomProgressIndicator.kt
@@ -1,5 +1,6 @@
 package com.wantech.noshame.featureMenstrualTrack.presentation.home.components
 
+import android.content.Context
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
@@ -18,21 +19,28 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalFontFamilyResolver
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextMeasurer
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.*
 import com.wantech.noshame.ui.theme.WhiteColor
 
+@OptIn(ExperimentalTextApi::class, ExperimentalUnitApi::class)
 @Composable
 fun CustomProgressIndicator(
-    canvasSize: Dp = 300.dp,
+    canvasSize: Dp = 200.dp,
     indicatorValue: Int = 0,
     maxiMumIndicatorValue: Int = 30,
     backgroundIndicatorColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f),
     backgroundIndicatorStrokeWidth: Float = 20f,
     foregroundIndicatorColor: Color = Color.Blue,
     foregroundIndicatorStrokeWidth: Float = 20f,
-    modifier: Modifier
+    modifier: Modifier,
+    context: Context
 
 ) {
     var animatedIndicatorValue by remember {
@@ -52,12 +60,39 @@ fun CustomProgressIndicator(
         targetValue = (12 * percentage),
         animationSpec = tween(durationMillis = 1000)
     )
+    val fontResolver = LocalFontFamilyResolver.current
+    val fallbackDensity = Density(context = context)
+    val fallBackLayoutDirection = LayoutDirection.Ltr
+    val mStyle = MaterialTheme.typography.headlineLarge.copy(
+        color = WhiteColor,
+        fontWeight = FontWeight.Thin,
+        fontSize = TextUnit(18f, type = TextUnitType.Sp)
+    )
+    MaterialTheme.colorScheme.background
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = modifier
             .size(canvasSize)
             .drawBehind {
+                val myComponentSize by lazy {
+                    canvasSize.value / 1.25f
+                }
+                drawText(
+                    text = "Next Cycle in",
+                    textMeasurer = TextMeasurer(
+                        fallbackFontFamilyResolver = fontResolver,
+                        fallbackDensity = fallbackDensity,
+                        fallbackLayoutDirection = fallBackLayoutDirection,
+                    ),
+                    style = mStyle,
+                    topLeft = Offset(
+                        x = ((canvasSize.value - myComponentSize) / 2) + 40,
+                        y = (canvasSize.value - (myComponentSize + 60))
+                    )
+                )
+
+
                 val componentSize = size / 1.25f
                 backgroundIndicator(
                     indicatorStrokeWidth = backgroundIndicatorStrokeWidth,
@@ -158,12 +193,3 @@ fun EmbeddedElements(
 }
 
 
-@Preview(
-    showBackground = true,
-//    showSystemUi = true
-)
-@Composable
-private fun CustomProgressIndicatorPreview() {
-
-    CustomProgressIndicator(modifier = Modifier)
-}
