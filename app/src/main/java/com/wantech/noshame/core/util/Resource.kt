1e68070e321c0045cba4d3a972010ec0d4024120
wanjala stephen <stephenwanjala145@gmail.com>
2023-01-01 15:01:49 +0300

    utilities

---
+++ app/src/main/java/com/wantech/noshame/core/util/Resource.kt
@@ -0,0 +1,9 @@
+package com.wantech.noshame.core.util
+
+typealias SimpleResource = Resource<Unit>
+
+sealed class Resource<T>(val data: T? = null, val uiText: UiText? = null) {
+    class Success<T>(data: T?): Resource<T>(data)
+    class Error<T>(uiText: UiText, data: T? = null): Resource<T>(data, uiText)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+}
---
+++ app/src/main/java/com/wantech/noshame/core/util/UiText.kt
@@ -0,0 +1,15 @@
+package com.wantech.noshame.core.util
+
+import androidx.annotation.StringRes
+import com.wantech.noshame.R
+
+sealed class UiText {
+    data class DynamicString(val value: String): UiText()
+    data class StringResource(@StringRes val id: Int): UiText()
+
+    companion object {
+        fun unknownError(): UiText {
+            return UiText.StringResource(R.string.error_unknown)
+        }
+    }
+}
