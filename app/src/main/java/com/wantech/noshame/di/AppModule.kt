3e1e10adf7447fa9e5064acc61e379889ec39d80
wanjala stephen <stephenwanjala145@gmail.com>
2023-01-09 16:48:12 +0300

    provide Auth repository Impl and firebaseAuth

--- app/src/main/java/com/wantech/noshame/di/AppModule.kt
+++ app/src/main/java/com/wantech/noshame/di/AppModule.kt
@@ -1,9 +1,23 @@
 package com.wantech.noshame.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.wantech.noshame.feature_auth.data.repositoryImpl.AuthRepositoryImpl
+import com.wantech.noshame.feature_auth.domain.repository.AuthRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(auth: FirebaseAuth): AuthRepository = AuthRepositoryImpl(auth = auth)
+}
