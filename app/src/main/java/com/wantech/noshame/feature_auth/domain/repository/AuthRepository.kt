b2f2d179664e3f3904ea8dc8990930dd5e690090
wanjala stephen <stephenwanjala145@gmail.com>
2023-01-09 16:48:54 +0300

    SignUpViewModel Up

--- app/src/main/java/com/wantech/noshame/feature_auth/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/wantech/noshame/feature_auth/domain/repository/AuthRepository.kt
@@ -1,10 +1,21 @@
 package com.wantech.noshame.feature_auth.domain.repository
 
+import com.google.firebase.auth.AuthResult
+import com.wantech.noshame.core.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
-    suspend fun signInUserWithEmailAndPassword(email: String, password: String)
-    suspend fun createUserWithEmailAndPassword(email: String, password: String, userName: String)
+    suspend fun signInUserWithEmailAndPassword(
+        email: String,
+        password: String
+    ): Flow<Resource<AuthResult>>
+
+    suspend fun createUserWithEmailAndPassword(
+        email: String,
+        password: String,
+        userName: String
+    ): Flow<Resource<AuthResult>>
+
     fun isCurrentUserExist(): Flow<Boolean>
 
     suspend fun logoutUser()
--- app/src/main/java/com/wantech/noshame/feature_auth/presentation/signUp/SignUpViewModel.kt
+++ app/src/main/java/com/wantech/noshame/feature_auth/presentation/signUp/SignUpViewModel.kt
@@ -3,17 +3,22 @@ package com.wantech.noshame.feature_auth.presentation.signUp
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wantech.noshame.core.util.Resource
+import com.wantech.noshame.feature_auth.domain.repository.AuthRepository
 import com.wantech.noshame.feature_auth.presentation.signUp.components.SignUpState
 import com.wantech.noshame.feature_auth.presentation.signUp.components.SignUpUIState
 import com.wantech.noshame.feature_auth.presentation.signUp.components.SignupEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SignUpViewModel @Inject constructor(
-//    private val repository: AuthRepository
+    private val repository: AuthRepository
 
 ) : ViewModel() {
     private val _state = mutableStateOf(SignUpUIState())
@@ -23,34 +28,50 @@ class SignUpViewModel @Inject constructor(
     val signUpIState = _signUpUIState.asSharedFlow()
 
 
-    fun onEvent(event: SignupEvent) {
-        when (event) {
-            is SignupEvent.ChooseLengthCycle -> {
-                _state.value = state.value.copy(dayOneOfPreviousPeriod = event.spinnerData.id)
-            }
-            is SignupEvent.EnteredEmail -> {
-                _state.value = state.value.copy(email = event.value)
-            }
-            is SignupEvent.EnteredPassword -> {
-                _state.value = state.value.copy(password = event.value)
-            }
-            is SignupEvent.EnteredUsername -> {
-                _state.value = state.value.copy(userName = event.value)
-            }
-            SignupEvent.Signup -> {
-
-            }
-            SignupEvent.ToSignInScreen -> {
-
-            }
-            SignupEvent.TogglePasswordVisibility -> {
-                _state.value = state.value.copy(isPasswordVisible = !state.value.isPasswordVisible)
-            }
+    fun onEvent(event: SignupEvent) = when (event) {
+        is SignupEvent.ChooseLengthCycle -> {
+            _state.value = state.value.copy(dayOneOfPreviousPeriod = event.spinnerData.id)
         }
+        is SignupEvent.EnteredEmail -> {
+            _state.value = state.value.copy(email = event.value)
+        }
+        is SignupEvent.EnteredPassword -> {
+            _state.value = state.value.copy(password = event.value)
+        }
+        is SignupEvent.EnteredUsername -> {
+            _state.value = state.value.copy(userName = event.value)
+        }
+        SignupEvent.Signup -> {
+            signUp(
+                userName = state.value.userName.trim(),
+                email = state.value.email.trim(),
+                password = state.value.password.trim()
+            )
 
+        }
+        SignupEvent.TogglePasswordVisibility -> {
+            _state.value = state.value.copy(isPasswordVisible = !state.value.isPasswordVisible)
+        }
     }
 
 
-
-    private fun signUp(userName: String, email: String, password: String){}
+    private fun signUp(userName: String, email: String, password: String) {
+        viewModelScope.launch {
+            repository.createUserWithEmailAndPassword(
+                email = userName, password = email, userName = password
+            ).onEach { resource ->
+                when (resource) {
+                    is Resource.Error -> {
+                        _signUpUIState.emit(SignUpState(error = resource.uiText))
+                    }
+                    is Resource.Loading -> {
+                        _signUpUIState.emit(SignUpState(isLoading = true))
+                    }
+                    is Resource.Success -> {
+                        _signUpUIState.emit(SignUpState(signUp = resource.data))
+                    }
+                }
+            }
+        }
+    }
 }
