20e9bbf0168a6d891cf2483eccc127d24844a004
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-07-17 11:30:58 +0300

    prvide Auth Api

--- app/src/main/java/com/wantech/noshame/di/AppModule.kt
+++ app/src/main/java/com/wantech/noshame/di/AppModule.kt
@@ -1,11 +1,16 @@
 package com.wantech.noshame.di
 
+import com.wantech.noshame.feature_auth.data.network.AuthApi
 import com.wantech.noshame.feature_auth.data.repositoryImpl.AuthRepositoryImpl
 import com.wantech.noshame.feature_auth.domain.repository.AuthRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -13,6 +18,36 @@ import javax.inject.Singleton
 object AppModule {
 
 
+
+
+    private const val BaseUrl = "http://192.168.1.148:8080/"
+
+    @Singleton
+    @Provides
+    fun providesHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BaseUrl)
+        .client(okHttpClient)
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): AuthApi = retrofit.create(AuthApi::class.java)
 
     @Provides
     @Singleton
